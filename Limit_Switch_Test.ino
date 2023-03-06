8ddef8fb78704af8cb1dc2dea4e78cfe3b220523
Noah <79220230+nabushanab@users.noreply.github.com>
2023-03-06 12:56:23 -0600

    Update Limit_Switch_Test.ino
    almost works

--- Limit_Switch_Test.ino
+++ Limit_Switch_Test.ino
@@ -18,20 +18,21 @@ const int limitY = 7;
 AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
 AccelStepper myStepper1(motorInterfaceType, stepPin1, dirPin1);
 
-int moveAmount = 500;
-int currentPos = 1;
+int moveAmount = 50;
 
 void setup() {
-  // set the maximum speed, acceleration factor,
-  // initial speed and the target position
-  myStepper.setMaxSpeed(1000);
+  //setup serial monitor for testing
+  Serial.begin(9600);
+
+
+  myStepper.setMaxSpeed(500);
   myStepper.setAcceleration(250);
-  myStepper.setSpeed(200);
+  myStepper.setSpeed(500);
 
 
-  myStepper1.setMaxSpeed(1000);
+  myStepper1.setMaxSpeed(500);
   myStepper1.setAcceleration(250);
-  myStepper1.setSpeed(200);
+  myStepper1.setSpeed(500);
 
   pinMode(limitX, INPUT);
   pinMode(limitY, INPUT);
@@ -46,61 +47,66 @@ void loop() {
 
 //Move right 
 void dirRight(){
-  myStepper.moveTo(moveAmount);
-  myStepper1.moveTo(moveAmount);
+  myStepper.move(moveAmount);
+  myStepper1.move(moveAmount);
 }
 
 //Move up
 void dirUp(){
-  myStepper.moveTo(-moveAmount);
-  myStepper1.moveTo(moveAmount);
+  myStepper.move(-moveAmount);
+  myStepper1.move(moveAmount);
 }
 
 //Move Down
 void dirDown(){
-  myStepper.moveTo(moveAmount);
-  myStepper1.moveTo(-moveAmount);
+  myStepper.move(moveAmount);
+  myStepper1.move(-moveAmount);
 }
 
 //Move Left
 void dirLeft(){
-  myStepper.moveTo(-moveAmount);
-  myStepper1.moveTo(-moveAmount);
+  myStepper.move(-moveAmount);
+  myStepper1.move(-moveAmount);
+}
+
+//complete movement
+void completeMovement(){
+  while (myStepper.distanceToGo() != 0 && myStepper1.distanceToGo() != 0){
+      myStepper.run();
+      myStepper1.run();
+  }
 }
 
 
 
+
 void calibrate(){
-  int calStep = 0;
-  //keep moving until we hit both limit switches
-  while (digitalRead(limitY) == LOW && digitalRead(limitX) == LOW){
-    moveAmount = 100;
-      
-      //move left a lil for the first step
-      if(calStep == 0){
-        dirLeft();
-        calStep = 1;
-      }
-
-      //now move up until we hit the limit switch
-      if(calStep == 1){
-        dirUp();
-        //now check if limit switch is on to see if we need to switch to moving right
-        if(digitalRead(limitY) == HIGH){
-          dirRight();
-          if(digitalRead(limitX) == HIGH){
-            calStep = 3;
-            }
-        }
-      }
-
- 
-        //now check if limit switch is on
-       
-      
-
-    myStepper.run();
-    myStepper1.run();
-
+  if(digitalRead(limitY) == LOW && digitalRead(limitX) == LOW){
+  //move up until we hit limitY
+  calibrateY();
   }
+
+  if(digitalRead(limitY) == HIGH && digitalRead(limitX) == LOW){
+  //move Right until we hit limitX
+  calibrateX();
+  }
+  
+}
+
+
+
+void calibrateY(){
+  while(digitalRead(limitY) == LOW){
+    dirUp();
+    completeMovement();
+  }
+  return;
+}
+
+void calibrateX(){
+  while(digitalRead(limitX) == LOW){
+    dirRight();
+    completeMovement();
+  }
+  return;
 }
